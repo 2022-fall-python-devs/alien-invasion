35b0412ad5b4e5728f0dcda786bc282b222414f0
chwalk <chwalker@madisoncollege.edu>
2022-10-24 15:11:54 -0500

    Added comment prior to application start.

--- alien_invasion.py
+++ alien_invasion.py
@@ -35,6 +35,7 @@ class AlienInvasion:
             # Make the most recently drawn screen visible.
             pygame.display.flip()
 
+# The application starts here.
 if __name__ == '__main__':
     # Make a game instance, and run the game.
     ai = AlienInvasion()
