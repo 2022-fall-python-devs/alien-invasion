d6cab399b23a1b1d5183e4687cc993182e8a1ce4
carywalk <carywalk@yahoo.com>
2022-10-24 15:43:04 -0500

    Updated comment.

--- alien_invasion.py
+++ alien_invasion.py
@@ -35,7 +35,7 @@ class AlienInvasion:
             # Make the most recently drawn screen visible.
             pygame.display.flip()
 
-# Re-typing the application starts here.
+# The application starts here.
 if __name__ == '__main__':
     # Make a game instance, and run the game.
     ai = AlienInvasion()
